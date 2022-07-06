5eff339232eb2ed8de4cc5f8ef87da823e1b6a2d
Poleeknow <106336793+Poleeknow@users.noreply.github.com>
2022-07-06 21:15:17 +0000

    Deploying to gh-pages from @ Poleeknow/Poleeknow.github.io@31b5c8d9a94bdc63a89cb9608628b16f50d94be8 🚀

---
+++ _app/immutable/pages/about_me.svelte-99e15638.js
@@ -0,0 +1,17 @@
+import{S as A,i as E,s as H,e as c,t as b,k as _,c as m,a as u,h as I,d as r,m as w,b as z,f as e,g as G,F as o,n as f}from"../chunks/index-c54306ee.js";function R(P){let a,t,d,h,n,p,g,i,y;return{c(){a=c("main"),t=c("p"),d=b("Hallo, ich hei\xDFe Polina..."),h=_(),n=c("p"),p=b(`Hello, my name is Polina. I'm 18 and I have been studying German.
+        Actually, my mother language is Russian, and also, I can speak English not bad.
+        Here is some curious facts about me.`),g=_(),i=c("p"),y=b(`I'm from small town in Udmurt Republic named Glazov,
+        but now I have been studying and living in Innopolis.
+        I used to do archery, but right now I haven't such possibility.
+        Maybe, with time I can realise the idea about "archery club" in Innopolis.
+        Besides that, I like swimming as well as figure skating. What else...
+        According to the horoscope, I'm Libra.
+        I really like cooking and baking, especially making tasty cakes or biscuits.`),this.h()},l(l){a=m(l,"MAIN",{});var s=u(a);t=m(s,"P",{id:!0,style:!0});var k=u(t);d=I(k,"Hallo, ich hei\xDFe Polina..."),k.forEach(r),h=w(s),n=m(s,"P",{style:!0});var v=u(n);p=I(v,`Hello, my name is Polina. I'm 18 and I have been studying German.
+        Actually, my mother language is Russian, and also, I can speak English not bad.
+        Here is some curious facts about me.`),v.forEach(r),g=w(s),i=m(s,"P",{style:!0});var x=u(i);y=I(x,`I'm from small town in Udmurt Republic named Glazov,
+        but now I have been studying and living in Innopolis.
+        I used to do archery, but right now I haven't such possibility.
+        Maybe, with time I can realise the idea about "archery club" in Innopolis.
+        Besides that, I like swimming as well as figure skating. What else...
+        According to the horoscope, I'm Libra.
+        I really like cooking and baking, especially making tasty cakes or biscuits.`),x.forEach(r),s.forEach(r),this.h()},h(){z(t,"id","ddd"),e(t,"margin","30px"),e(t,"color","darkviolet"),e(t,"text-align","justify"),e(t,"font-size","30px"),e(t,"text-align","center"),e(n,"margin","30px"),e(n,"color","darkviolet"),e(n,"text-align","justify"),e(n,"font-size","30px"),e(i,"margin","30px"),e(i,"color","darkviolet"),e(i,"text-align","justify"),e(i,"font-size","30px")},m(l,s){G(l,a,s),o(a,t),o(t,d),o(a,h),o(a,n),o(n,p),o(a,g),o(a,i),o(i,y)},p:f,i:f,o:f,d(l){l&&r(a)}}}class M extends A{constructor(a){super(),E(this,a,null,R,H,{})}}export{M as default};

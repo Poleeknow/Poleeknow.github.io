6a7c13e9828715c1ffdf505bea7d232cbf573b1c
Poleeknow <8490743@gmail.com>
2022-07-06 23:19:58 +0300

    uwu

--- src/routes/about_me.svelte
+++ src/routes/about_me.svelte
@@ -1,18 +1,3 @@
-<main>
-    <p id="ddd">Hallo, ich heiße Polina...</p>
-    <p>Hello, my name is Polina. I'm 18 and I have been studying German.
-        Actually, my mother language is Russian, and also, I can speak English not bad.
-        Here is some curious facts about me.</p>
-    <p>I'm from small town in Udmurt Republic named Glazov,
-        but now I have been studying and living in Innopolis.
-        I used to do archery, but right now I haven't such possibility.
-        Maybe, with time I can realise the idea about "archery club" in Innopolis.
-        Besides that, I like swimming as well as figure skating. What else...
-        According to the horoscope, I'm Libra.
-        I really like cooking and baking, especially making tasty cakes or biscuits.
-    </p>
-</main>
-
 <style>
     header {
         background-color: lavenderblush;
@@ -49,3 +34,18 @@
         font-size: 18px;
     }
 </style>
+<main>
+    <p id="ddd">Hallo, ich heiße Polina...</p>
+    <p>Hello, my name is Polina. I'm 18 and I have been studying German.
+        Actually, my mother language is Russian, and also, I can speak English not bad.
+        Here is some curious facts about me.</p>
+    <p>I'm from small town in Udmurt Republic named Glazov,
+        but now I have been studying and living in Innopolis.
+        I used to do archery, but right now I haven't such possibility.
+        Maybe, with time I can realise the idea about "archery club" in Innopolis.
+        Besides that, I like swimming as well as figure skating. What else...
+        According to the horoscope, I'm Libra.
+        I really like cooking and baking, especially making tasty cakes or biscuits.
+    </p>
+</main>
+

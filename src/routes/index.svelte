a7406802d72f513ac9c949bf097e0de642fd5d18
Poleeknow <8490743@gmail.com>
2022-07-07 00:10:46 +0300

    fix

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -38,3 +38,41 @@
     </div>
     <div id="two"><img id="dog" alt="Some dog" src=""></div>
 </div>
+<style>
+    .flexbox {
+        display: flex;
+        margin-left: 30px;
+        margin-right: 30px;
+        margin-top: 10px;
+    }
+
+    #two {
+        display: none;
+        position: relative;
+        margin-left: 10%;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        border: none;
+    }
+    #me {
+        width: 100%;
+    }
+    #dog {
+        object-fit: cover;
+        width: 92%;
+        height: 69%;
+    }
+    #getDogImg {
+        font-size: 300%;
+        border-radius: 50%;
+        background-color: lavenderblush;
+        color: hotpink;
+        border: 2px solid hotpink;
+        transition-duration: 0.4s;
+    }
+    #getDogImg:hover {
+        background-color: hotpink;
+        color: lavenderblush;
+    }
+</style>

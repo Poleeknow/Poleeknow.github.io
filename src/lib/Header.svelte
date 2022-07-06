c51fe385c7afe70a8c88a0d6c208c58598d16b56
Poleeknow <8490743@gmail.com>
2022-07-06 22:45:16 +0300

    hw5

---
+++ src/lib/Header.svelte
@@ -0,0 +1,51 @@
+
+
+<header>
+    <a href="/" style="text-decoration: none !important; border: none !important"><h1 id="header" >POLINA BAZHENOVA</h1></a>
+    <nav>
+        <a class="menu" href= "/about_me"
+                style ="width: auto;
+                color: hotpink;
+                text-decoration: none;
+                text-align: center;
+                font-size: 24px;"> About me</a>
+        <a class="menu" href= "/projects"
+                style ="width: auto;
+                color: hotpink;
+                text-decoration: none;
+                text-align: center;
+                font-size: 24px;"> My projects </a>
+        <a class="menu" href="mailto:8490743@gmail.com"> Contact me</a>
+    </nav>
+</header>
+
+
+<style> 
+    header {
+        background-color: lavenderblush;
+        border: none;
+        margin: 0;
+        overflow: hidden;
+        width: 100%;
+    }
+    .menu{
+            width: auto;
+            color: hotpink;
+            text-decoration: none;
+            text-align: center;
+            font-size: 24px;
+    }
+    h1 {
+        color: hotpink;
+        margin: 30px 30px 20px 30px;
+        text-align: left;
+            font-size: 48px;
+    }
+    nav {
+            margin-bottom: 10px;
+            width: 100%;
+            display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    }
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Header from '../lib/Header.svelte';
+	import Links from "../lib/Links.svelte";
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<Links/>
